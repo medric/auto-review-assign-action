f8c3fe93138d16c001c5b935991dbeb5c238e9d4
medric <medric.chouan@gmail.com>
2020-05-18 22:39:06 +1000

    assign reviewer to PR based on input parameter

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,42 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { Context } from "@actions/github/lib/context";
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+const createPullRequest = (client: any, context: Context) => {
+  const { pull_request: pr } = context.payload;
+
+  if (!pr) {
+    core.error("Event payload missing `pull_request`");
+    return;
+  }
+
+  return {
+    async addReviewers(reviewers: string[]) {
+      const { owner, repo, number: pull_number } = context.issue;
+      const result = await client.pulls.createReviewRequest({
+        owner,
+        repo,
+        pull_number,
+        reviewers,
+      });
+      core.debug(JSON.stringify(result));
+    },
+  };
+};
+
+export async function run() {
+  try {
+    const token = core.getInput("token", { required: true });
+    const reviewers = core.getInput("reviewers", { required: true }).split(",");
+
+    const client = new github.GitHub(token);
+
+    const pr = createPullRequest(client, github.context);
+
+    pr?.addReviewers(reviewers);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
