8bd4f84b6e2d44dd7da1216171430fc20d18acf4
medric <medric.chouan@gmail.com>
2020-06-02 15:10:03 +1000

    dummy test

--- src/utils.ts
+++ src/utils.ts
@@ -2,16 +2,16 @@ import { Octokit } from "@octokit/rest";
 
 export const allChecksSuccess = (
   checkRuns: Octokit.ChecksListForRefResponseCheckRunsItem[]
-) =>
-  checkRuns.every(
-    (checkRun: Octokit.ChecksListForRefResponseCheckRunsItem) =>
-      checkRun.conclusion === "success"
-  );
+) => true;
+  // checkRuns.every(
+  //   (checkRun: Octokit.ChecksListForRefResponseCheckRunsItem) =>
+  //     checkRun.conclusion === "success"
+  // );
 
 export const allChecksCompleted = (
   checkRuns: Octokit.ChecksListForRefResponseCheckRunsItem[]
-) =>
-  checkRuns.every(
-    (checkRun: Octokit.ChecksListForRefResponseCheckRunsItem) =>
-      checkRun.status === "completed"
-  );
+) => true;
+  // checkRuns.every(
+  //   (checkRun: Octokit.ChecksListForRefResponseCheckRunsItem) =>
+  //     checkRun.status === "completed"
+  // );
